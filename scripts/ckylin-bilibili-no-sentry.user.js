17aa445af5c44ba1ab03f8d0bd49db500ab0f03c
CKylinMC <11648014+CKylinMC@users.noreply.github.com>
2021-12-08 17:51:11 +0800

    Update ckylin-bilibili-no-sentry.user.js

--- scripts/ckylin-bilibili-no-sentry.user.js
+++ scripts/ckylin-bilibili-no-sentry.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         [Bilibili] 不要Sentry!
 // @namespace    ckylin-bilibili-no-sentry
-// @version      1.0
+// @version      1.1
 // @description  不要Sentry!
 // @author       CKylinMC
-// @match        https://www.bilibili.com/video*
+// @match        https://www.bilibili.com/*
+// @match        https://space.bilibili.com/*
+// @match        https://t.bilibili.com/*
 // @grant        unsafeWindow
 // @license      GPL-3.0-only
 // @run-at       document-start
