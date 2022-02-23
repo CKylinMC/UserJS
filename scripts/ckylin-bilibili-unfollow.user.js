7c8abc837648ea2a93d527d109613d73eda868c3
CKylinMC <canszj@yeah.net>
2022-02-23 07:47:12 +0000

    chore(unfollow): bump version code

--- scripts/ckylin-bilibili-unfollow.user.js
+++ scripts/ckylin-bilibili-unfollow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Bilibili] 关注管理器
 // @namespace    ckylin-bilibili-manager
-// @version      0.2.9
+// @version      0.2.10
 // @description  快速排序和筛选你的关注列表，一键取关不再关注的UP等
 // @author       CKylinMC
 // @updateURL    https://cdn.jsdelivr.net/gh/CKylinMC/UserJS/scripts/ckylin-bilibili-unfollow.user.js
@@ -46,7 +46,7 @@
     const cfg = {
         debug: false,
         retrial: 3,
-        VERSION: "0.2.9 Beta",
+        VERSION: "0.2.10 Beta",
         infobarTemplate: ()=>`共读取 ${datas.fetched} 条关注`,
         titleTemplate: ()=>`<h1>关注管理器 <small>v${cfg.VERSION} ${cfg.debug?"debug":""}</small></h1>`
     }
