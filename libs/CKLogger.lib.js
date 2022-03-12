b2b2c06bb6ba3ef235be719590e31303bf635182
CKylinMC <11648014+CKylinMC@users.noreply.github.com>
2022-03-12 16:11:32 +0800

    Update CKLogger.lib.js

--- libs/CKLogger.lib.js
+++ libs/CKLogger.lib.js
@@ -21,7 +21,42 @@
 			this.parent = parent;
 			this.visible = this.parent?null:true;
 			this.serviceProvider = console;
+            this.subscribers = [];
 		}
+        trigger(opt){
+            this.subscribers.filter(subscriber=>{
+                if(!this.isVisible() || !opt.visible){
+                    if(subscriber.onlyVisible)return false;
+                }
+                if(opt.fromSub){
+                    if(!subscriber.includeSubLoggers)return false;
+                }
+                return subscriber.levels.includes(opt.level);
+            }).forEach(subscriber=>{
+                try{
+                    subscriber.onlog(opt.level,opt.name,...opt.args);
+                }catch(e){}
+            });
+            if(this.parent){
+                opt.fromSub = true;
+                this.parent.trigger(opt);
+            }
+        }
+        subscribe(options){
+            const opt = Object.assign({
+                onlyVisible: false,
+                includeSubLoggers: true,
+                levels: ['log','info','error','warn','debug'],
+                onlog: (level,namespace,...args)=>{}
+            },options);
+            this.subscribers.push(opt);
+            return opt;
+        }
+        unsubscribe(obj){
+            if(this.subscribers.includes(obj)){
+                return this.subscribers.splice(this.subscribers.indexOf(obj),1);
+            } return null;
+        }
 		isVisible(){
 			if(this.visible===null){
 				if(this.parent&&this.parent.isVisible){
@@ -43,6 +78,13 @@
 			return `[${this.getName()}]`
 		}
 		sendLogs(subMethod,...contents){
+            this.trigger({
+                level: 'raw',
+                fromSub: false,
+                visible: this.isVisible(),
+                name: this.getName(),
+                args: contents
+            });
 			if(this.isVisible()){
 				if(this.serviceProvider.hasOwnProperty(subMethod)){
 					this.serviceProvider[subMethod](...contents);
@@ -52,24 +94,66 @@
 			return false;
 		}
 		log(...args){
+            this.trigger({
+                level: 'log',
+                fromSub: false,
+                visible: this.isVisible(),
+                name: this.getName(),
+                args: args
+            });
 			return this.sendLogs('log',this.getFormattedName(),...args);
 		}
 		info(...args){
+            this.trigger({
+                level: 'info',
+                fromSub: false,
+                visible: this.isVisible(),
+                name: this.getName(),
+                args: args
+            });
 			return this.sendLogs('info',this.getFormattedName(),...args);
 		}
 		warn(...args){
+            this.trigger({
+                level: 'warn',
+                fromSub: false,
+                visible: this.isVisible(),
+                name: this.getName(),
+                args: args
+            });
 			const stacks = '\nTrace:\n'+Logger.getStack().split('\n').splice(2).join('\n');
 			return this.sendLogs('warn',this.getFormattedName(),...args,stacks);
 		}
 		error(...args){
+            this.trigger({
+                level: 'error',
+                fromSub: false,
+                visible: this.isVisible(),
+                name: this.getName(),
+                args: args
+            });
 			const stacks = '\nTrace:\n'+Logger.getStack().split('\n').splice(2).join('\n');
 			return this.sendLogs('error',this.getFormattedName(),...args,stacks);
 		}
 		debug(...args){
+            this.trigger({
+                level: 'debug',
+                fromSub: false,
+                visible: this.isVisible(),
+                name: this.getName(),
+                args: args
+            });
 			const stacks = '\nTrace:\n'+Logger.getStack().split('\n').splice(2).join('\n');
 			return this.sendLogs('debug',this.getFormattedName(),...args,stacks);
 		}
 		send(methodName,...args){
+            this.trigger({
+                level: methodName,
+                fromSub: false,
+                visible: this.isVisible(),
+                name: this.getName(),
+                args: args
+            });
 			return this.sendLogs(methodName,this.getFormattedName(),...args);
 		}
 	}
