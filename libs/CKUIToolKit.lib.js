88cb73504bb6642f1ce1f46123df0bb14a112494
CKylinMC <canszj@yeah.net>
2022-03-20 17:15:47 +0800

    add alternative load method for resources

--- libs/CKUIToolKit.lib.js
+++ libs/CKUIToolKit.lib.js
@@ -19,196 +19,214 @@
     if (typeof (unsafeWindow) == 'undefined') {
         unsafeWindow = window;
     }
-    if (typeof (GM_getResourceText) != 'undefined') { 
-        //======[Apply all resources]
-        const resourceList = [
-            { name: 'popjs', type: 'js' },
-            { name: 'popcss', type: 'css' },
-            { name: 'fpjs', type: 'js' },
-            { name: 'fpcss', type: 'css' },
-            { name: 'popcsspatch', type: 'rawcss', content: "div.popNotifyUnitFrame{z-index:110000!important;}.CKTOOLS-modal-content{color: #616161!important;max-height: 80vh;overflow: auto;}" },
-            { name: 'settingscss', type: 'rawcss', content: `
-                .ckui-base .ckui-text{
-                    font-size: 14px;
-                    line-height: 1.428571429;
-                }
-                .ckui-base label{
-                    display: block;
-                    color: rgb(16, 140, 255);
-                    padding-top: 12px;
-                }
-                .ckui-base .ckui-texttoggle{
-                    display: block;
-                    color: rgb(16, 140, 255);
-                    padding-top: 12px;
-                }
-                .ckui-base .ckui-texttoggle-container::before{
-                    display: inline;
-                    content: "🔹";
-                }
-                .ckui-base .ckui-texttoggle-container .ckui-texttoggle-value{
-                    padding: 0px 6px;
-                    font-weight: bold;
-                    -webkit-user-select: none;
-                    -moz-user-select: none;
-                    -ms-user-select: none;
-                    user-select: none;
-                    cursor: pointer;
-                }
-                .ckui-base .ckui-texttoggle-container .ckui-texttoggle-value::before{
-                    content: "["
-                }
-                .ckui-base .ckui-texttoggle-container .ckui-texttoggle-value::after{
-                    content: "]"
-                }
-                .ckui-base .ckui-texttoggle-container::after{
-                    display: inline;
-                    content: "(点击切换)";
-                    color: gray;
-                    font-size: 12px;
-                    font-style: italic;
-                    padding-left: 12px;
-                }
-                .ckui-base .ckui-description{
-                    display: block;
-                    color: rgb(92, 92, 92);
-                    font-size: small;
-                    font-style: italic;
-                }
-                .ckui-base .ckui-toggle{
-                    padding-top: 12px;
-                }
-                .ckui-base label.ckui-inline-label{
-                    display: inline;
-                    line-height: 18px;
-                }
-                .ckui-base .ckui-input input{
-                    display: block;
-                    width: calc(100% - 28px);
-                    height: 34px;
-                    padding: 1px 3px;
-                    margin: 3px 6px;
-                    font-size: 14px;
-                    line-height: 1.428571429;
-                    color: rgb(51, 51, 51);
-                    background-color: rgb(255, 255, 255);
-                    border: 1px solid rgb(204, 204, 204);
-                    border-radius: 4px;
-                    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-                }
-                .ckui-base .ckui-inputnumber input{
-                    display: block;
-                    width: calc(100% - 36px);
-                    height: 34px;
-                    padding: 1px 12px;
-                    margin: 3px 6px;
-                    font-size: 14px;
-                    line-height: 1.428571429;
-                    color: rgb(51, 51, 51);
-                    background-color: rgb(255, 255, 255);
-                    border: 1px solid rgb(204, 204, 204);
-                    border-radius: 4px;
-                    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-                }
-                .ckui-base .ckui-inputarea textarea{
-                    display: block;
-                    width: calc(100% - 28px);
-                    height: 100px;
-                    padding: 6px 6px;
-                    margin: 3px 6px;
-                    font-size: 14px;
-                    line-height: 1.428571429;
-                    color: rgb(51, 51, 51);
-                    background-color: rgb(255, 255, 255);
-                    border: 1px solid rgb(204, 204, 204);
-                    border-radius: 4px;
-                    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-                }
-                .ckui-base .ckui-select select{
-                    display: block;
-                    width: calc(100% - 28px);
-                    height: 34px;
-                    padding: 6px 6px;
-                    margin: 3px 6px;
-                    font-size: 14px;
-                    line-height: 1.428571429;
-                    color: rgb(51, 51, 51);
-                    background-color: rgb(255, 255, 255);
-                    border: 1px solid rgb(204, 204, 204);
-                    border-radius: 4px;
-                    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-                }
-                .ckui-base .ckui-select select option{
-                    font-size: 16px;
-                    line-height: 1.428571429;
-                    color: rgb(51, 51, 51);
-                }
-                .ckui-base .ckui-header::before{
-                    content: "💠";
-                }
-                .ckui-base .ckui-header{
-                    width: calc(100% - 8px);
-                    display: block;
-                    color: rgb(16, 140, 255);
-                    padding: 12px 3px;
-                    border-bottom: 2px solid rgb(16, 140, 255);
-                    margin: 12px 0px 12px 0px;
-                }
-                .ckui-base .ckui-btns{
-                    display: flex;
-                    flex-wrap: wrap;
-                    flex-direction: row;
-                }
-                .ckui-base .ckui-btn{
-                    display: block;
-                    width: calc(50% - 8px);
-                    height: 40px;
-                    padding: 6px 12px;
-                    margin: 6px;
-                    font-size: 14px;
-                    line-height: 1.428571429;
-                    color: rgb(255, 255, 255);
-                    background-color: rgb(16, 140, 255);
-                    border: 1px solid rgb(16, 140, 255);
-                    border-radius: 4px;
-                    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-                }
-                .ckui-base .ckui-btn:hover{
-                    background-color: rgb(0, 122, 255);
-                }
-                .ckui-base .ckui-btns .ckui-btn{
-                    flex: 1;
-                }` }
-        ]
-        function applyResource() {
-            resloop: for (let res of resourceList) {
-                if (!document.querySelector("#" + res.name)) {
-                    let el;
-                    switch (res.type) {
-                        case 'js':
-                        case 'rawjs':
-                            el = document.createElement("script");
-                            break;
-                        case 'css':
-                        case 'rawcss':
-                            el = document.createElement("style");
-                            break;
-                        default:
-                            console.error('[CKUI]','Err:unknown type', res);
-                            continue resloop;
-                    }
-                    el.id = res.name;
-                    el.appendChild(document.createTextNode(res.type.startsWith('raw') ? res.content : GM_getResourceText(res.name)));
-                    document.head.appendChild(el);
-                    console.info('[CKUI]', 'Applied:',res.type,res.name);
-                }
-            }
-            console.info('[CKUI]', 'Resources all applied');
-        }
-        applyResource();
-    } else {
-        console.error('[CKUI]', 'Err:resource api not found');
+    if (typeof (GM_getResourceText) != 'function') {
+        GM_getResourceText = () => null;
     }
+    //======[Apply all resources]
+    const resourceList = [
+        { name: 'popjs', type: 'js', source: 'https://cdn.jsdelivr.net/gh/CKylinMC/PopNotify.js@master/PopNotify.js'},
+        { name: 'popcss', type: 'css', source:'https://cdn.jsdelivr.net/gh/CKylinMC/PopNotify.js@master/PopNotify.css' },
+        { name: 'fpjs', type: 'js', source: 'https://cdn.jsdelivr.net/gh/CKylinMC/FloatPopup.js@main/floatpopup.js' },
+        { name: 'fpcss', type: 'css', source: 'https://cdn.jsdelivr.net/gh/CKylinMC/FloatPopup.js@main/floatpopup.modal.css' },
+        { name: 'popcsspatch', type: 'rawcss', content: "div.popNotifyUnitFrame{z-index:110000!important;}.CKTOOLS-modal-content{color: #616161!important;max-height: 80vh;overflow: auto;}" },
+        { name: 'settingscss', type: 'rawcss', content: `
+            .ckui-base .ckui-text{
+                font-size: 14px;
+                line-height: 1.428571429;
+            }
+            .ckui-base label{
+                display: block;
+                color: rgb(16, 140, 255);
+                padding-top: 12px;
+            }
+            .ckui-base .ckui-texttoggle{
+                display: block;
+                color: rgb(16, 140, 255);
+                padding-top: 12px;
+            }
+            .ckui-base .ckui-texttoggle-container::before{
+                display: inline;
+                content: "🔹";
+            }
+            .ckui-base .ckui-texttoggle-container .ckui-texttoggle-value{
+                padding: 0px 6px;
+                font-weight: bold;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+                cursor: pointer;
+            }
+            .ckui-base .ckui-texttoggle-container .ckui-texttoggle-value::before{
+                content: "["
+            }
+            .ckui-base .ckui-texttoggle-container .ckui-texttoggle-value::after{
+                content: "]"
+            }
+            .ckui-base .ckui-texttoggle-container::after{
+                display: inline;
+                content: "(点击切换)";
+                color: gray;
+                font-size: 12px;
+                font-style: italic;
+                padding-left: 12px;
+            }
+            .ckui-base .ckui-description{
+                display: block;
+                color: rgb(92, 92, 92);
+                font-size: small;
+                font-style: italic;
+            }
+            .ckui-base .ckui-toggle{
+                padding-top: 12px;
+            }
+            .ckui-base label.ckui-inline-label{
+                display: inline;
+                line-height: 18px;
+            }
+            .ckui-base .ckui-input input{
+                display: block;
+                width: calc(100% - 28px);
+                height: 34px;
+                padding: 1px 3px;
+                margin: 3px 6px;
+                font-size: 14px;
+                line-height: 1.428571429;
+                color: rgb(51, 51, 51);
+                background-color: rgb(255, 255, 255);
+                border: 1px solid rgb(204, 204, 204);
+                border-radius: 4px;
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            }
+            .ckui-base .ckui-inputnumber input{
+                display: block;
+                width: calc(100% - 36px);
+                height: 34px;
+                padding: 1px 12px;
+                margin: 3px 6px;
+                font-size: 14px;
+                line-height: 1.428571429;
+                color: rgb(51, 51, 51);
+                background-color: rgb(255, 255, 255);
+                border: 1px solid rgb(204, 204, 204);
+                border-radius: 4px;
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            }
+            .ckui-base .ckui-inputarea textarea{
+                display: block;
+                width: calc(100% - 28px);
+                height: 100px;
+                padding: 6px 6px;
+                margin: 3px 6px;
+                font-size: 14px;
+                line-height: 1.428571429;
+                color: rgb(51, 51, 51);
+                background-color: rgb(255, 255, 255);
+                border: 1px solid rgb(204, 204, 204);
+                border-radius: 4px;
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            }
+            .ckui-base .ckui-select select{
+                display: block;
+                width: calc(100% - 28px);
+                height: 34px;
+                padding: 6px 6px;
+                margin: 3px 6px;
+                font-size: 14px;
+                line-height: 1.428571429;
+                color: rgb(51, 51, 51);
+                background-color: rgb(255, 255, 255);
+                border: 1px solid rgb(204, 204, 204);
+                border-radius: 4px;
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            }
+            .ckui-base .ckui-select select option{
+                font-size: 16px;
+                line-height: 1.428571429;
+                color: rgb(51, 51, 51);
+            }
+            .ckui-base .ckui-header::before{
+                content: "💠";
+            }
+            .ckui-base .ckui-header{
+                width: calc(100% - 8px);
+                display: block;
+                color: rgb(16, 140, 255);
+                padding: 12px 3px;
+                border-bottom: 2px solid rgb(16, 140, 255);
+                margin: 12px 0px 12px 0px;
+            }
+            .ckui-base .ckui-btns{
+                display: flex;
+                flex-wrap: wrap;
+                flex-direction: row;
+            }
+            .ckui-base .ckui-btn{
+                display: block;
+                width: calc(50% - 8px);
+                height: 40px;
+                padding: 6px 12px;
+                margin: 6px;
+                font-size: 14px;
+                line-height: 1.428571429;
+                color: rgb(255, 255, 255);
+                background-color: rgb(16, 140, 255);
+                border: 1px solid rgb(16, 140, 255);
+                border-radius: 4px;
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            }
+            .ckui-base .ckui-btn:hover{
+                background-color: rgb(0, 122, 255);
+            }
+            .ckui-base .ckui-btns .ckui-btn{
+                flex: 1;
+            }` }
+    ]
+    async function applyResource() {
+        resloop: for (let res of resourceList) {
+            if (true||!document.querySelector("#" + res.name)) {
+                let el;
+                switch (res.type) {
+                    case 'js':
+                    case 'rawjs':
+                        el = document.createElement("script");
+                        break;
+                    case 'css':
+                    case 'rawcss':
+                        el = document.createElement("style");
+                        break;
+                    default:
+                        console.error('[CKUI]','Err:unknown type', res);
+                        continue resloop;
+                }
+                el.id = res.name;
+                let result = res.type.startsWith('raw') ? res.content : GM_getResourceText(res.name);
+                if (result == null || result == 'null' || typeof result === 'undefined') {
+                    console.info('[CKUI]', 'Alternative method is using:',res.type,res.name);
+                    if (!res.source) {
+                        console.info('[CKUI]', 'Failed to apply:',res.type,res.name);
+                        continue resloop;
+                    }
+                    try {
+                        let response = await fetch(res.source);
+                        if (!response.ok) {
+                            console.info('[CKUI]', 'Failed to apply:',res.type,res.name,response.statusText);
+                            continue resloop;
+                        }
+                        result = await response.text();
+                    } catch (e) {
+                        console.info('[CKUI]', 'Failed to apply:',res.type,res.name,e);
+                        continue resloop;
+                    }
+                }
+                el.appendChild(document.createTextNode(result));
+                document.head.appendChild(el);
+                console.info('[CKUI]', 'Applied:',res.type,res.name);
+            }
+        }
+        console.info('[CKUI]', 'Resources all applied');
+    }
+    applyResource();
     
     let { domHelper, deepClone } = CKTools;
     if (!deepClone) {
